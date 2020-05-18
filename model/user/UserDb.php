9856489c39964bb4c6472b139a7cc3bb6cf8c028
dinhthanhtung2411 <dinhthanhtung2411@gmail.com>
2020-05-18 16:09:21 +0700

    function login

--- model/user/UserDb.php
+++ model/user/UserDb.php
@@ -21,4 +21,33 @@ class UserDb
         $stmt->execute();
     }
 
+    public function getValue()
+    {
+        $sql = "SELECT * FROM users";
+        $stmt = $this->conn->query($sql);
+        $result = $stmt->fetchAll();
+        $arr = [];
+        foreach ($result as $item) {
+            $user = new User($item['email'], $item['password'], $item['role']);
+            array_push($arr, $user);
+        }
+        return $arr;
+    }
+
+    public function login($user)
+    {
+        $sql = "SELECT * FROM users WHERE email = ?";
+        $stmt = $this->conn->prepare($sql);
+        $stmt->bindParam(1, $user->getEmail());
+        $stmt->execute();
+        $result = $stmt->fetch();
+
+        if ($result["password"] == $user->getPassword){
+            header("Location: .././index.php");
+        }else{
+            return "Wrong email or password";
+        }
+
+    }
+
 }
